4ce662fe46b746b1681b471c8772c9c487e59ee4
Fernando <fga1990@gmail.com>
2021-10-22 21:01:29 -0500

    se agregan los primeros ejemplos de fundamentos de Java Script

---
+++ 01-FUNDAMENTOS/app.js
@@ -0,0 +1,16 @@
+// console.log(console.log('Hola mundo!'));
+
+let a = 10, b = 10, c = 10, d = 10, x = a + b + c + d, y = "Hola", z = "Mundo";
+
+console.log('%c Mis Variables con estilos CSS', 'color:yellow; font-wight: bold');
+console.log({ a });
+console.log({ b });
+console.warn({ c });
+console.log('Aqui');
+console.error({ d });
+console.log('Aqui');
+console.warn({ x });
+
+console.table({ a, b, c, d, x, y, z });
+
+document.write('<h1>ETIQUETA H1</h1>')
